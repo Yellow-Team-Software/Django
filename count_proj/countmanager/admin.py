1d944ea9c33c6d1acc32178cd4ea6f17a743ce6e
camden parsons <camden.parsons@gmail.com>
2020-02-22 16:38:58 -0500

    modification of admin site format

--- count_proj/countmanager/admin.py
+++ count_proj/countmanager/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
+admin.site.site_header= 'Boston Code Camp Attendace Manager'
+
 admin.site.register(Room)
 admin.site.register(Speaker)
 admin.site.register(Session)
