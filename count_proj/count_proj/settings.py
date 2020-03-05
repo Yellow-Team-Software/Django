6d275dbdeceee9cb3a8e5356339ea4a798e3e850
ConnorMacone <maconec@wit.edu>
2020-03-05 15:31:02 -0500

    Pointing Django to mysql database

--- count_proj/count_proj/settings.py
+++ count_proj/count_proj/settings.py
@@ -74,11 +74,14 @@ WSGI_APPLICATION = 'count_proj.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
