be49b9f1e97de4784d1ff5bd9ca83bcf8c8357ad
ConnorMacone <maconec@wit.edu>
2020-03-20 20:41:08 -0400

    Updating the password so everyones setting file is the same

--- count_proj/count_proj/settings.py
+++ count_proj/count_proj/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'test_db',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '3306',
     }
