ca699581253adab8d8ba5127db400f16e0fd931a
ConnorMacone <maconec@wit.edu>
2020-04-09 22:26:48 -0400

    Replacing SQLite database

--- count_proj/count_proj/settings.py
+++ count_proj/count_proj/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'count_proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ENTER_DATABASE_NAME_HERE',
+        'USER': 'root',
+        'PASSWORD': 'ENTER_DATABASE_HERE',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
